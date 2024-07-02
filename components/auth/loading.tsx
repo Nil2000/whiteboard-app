bc4ae9a93eca382b42f688de76e7ba864d426a9b
NIL2000 <nilabhra09b.net@gmail.com>
2024-07-02 15:19:41 +0530

    Clerk and Convex init done

---
+++ components/auth/loading.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export const Loading = () => {
+	return (
+		<div className="h-full w-full flex flex-col gap-y-4 justify-center items-center">
+			<Image
+				src="/logo.svg"
+				alt="Loading"
+				width={100}
+				height={100}
+				className="animate-pulse duration-700"
+			/>
+		</div>
+	);
+};
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+	providers: [
+		{
+			domain: "https://saving-duckling-53.clerk.accounts.dev",
+			applicationID: "convex",
+		},
+	],
+};
