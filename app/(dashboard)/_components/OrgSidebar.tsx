bdef3774ebcd0c990179c976df2a8dfb9594403b
NIL2000 <nilabhra09b.net@gmail.com>
2024-07-11 20:29:16 +0530

    Dashboard layout init

---
+++ app/(dashboard)/_components/OrgSidebar.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+export const OrgSidebar = () => {
+	return (
+		<div className="hidden lg:flex flex-col space-y-6 w-[260px] pl-5 pt-5">
+			OrgSidebar
+		</div>
+	);
+};
