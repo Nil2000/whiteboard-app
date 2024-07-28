b3ec893b3e5a5e42821a93c76a45b856e7841e06
NIL2000 <nilabhra09b.net@gmail.com>
2024-07-28 11:40:50 +0530

    Toolbar UI done

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,16 +1,72 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { ToolButton } from "./tool-button";
+import {
+	Circle,
+	MousePointer2,
+	Pencil,
+	Redo,
+	Square,
+	StickyNote,
+	Type,
+	Undo,
+} from "lucide-react";
 
 export const Toolbar = () => {
 	return (
 		<div className="absolute top-16 left-2 flex flex-col gap-y-4 rounded-md">
 			<div className="bg-white rounded-md p-1.5 flex flex-col gap-y-1 items-center shadow-md">
-				<div>Pencil</div>
-				<div>Square</div>
-				<div>Circle</div>
+				<ToolButton
+					icon={MousePointer2}
+					onClick={() => {}}
+					label="Select"
+					isActive={false}
+				/>
+				<ToolButton
+					icon={Type}
+					onClick={() => {}}
+					label="Text"
+					isActive={false}
+				/>
+				<ToolButton
+					icon={StickyNote}
+					onClick={() => {}}
+					label="Sticky note"
+					isActive={false}
+				/>
+				<ToolButton
+					icon={Square}
+					onClick={() => {}}
+					label="Rectangle"
+					isActive={false}
+				/>
+				<ToolButton
+					icon={Circle}
+					onClick={() => {}}
+					label="Ellipse"
+					isActive={false}
+				/>
+				<ToolButton
+					icon={Pencil}
+					onClick={() => {}}
+					label="Draw"
+					isActive={false}
+				/>
 			</div>
 			<div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-				<div>Undo</div>
-				<div>redo</div>
+				<ToolButton
+					icon={Undo}
+					onClick={() => {}}
+					label="Undo"
+					isActive={false}
+					isDisabled={true}
+				/>
+				<ToolButton
+					icon={Redo}
+					onClick={() => {}}
+					label="Redo"
+					isActive={false}
+					isDisabled={true}
+				/>
 			</div>
 		</div>
 	);
